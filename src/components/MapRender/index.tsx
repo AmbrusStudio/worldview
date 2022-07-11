6e822310745339bc1d82e715b865307c7a0484e0
xiaotian <istianlei@qq.com>
2022-07-11 17:10:59 +0800

    feat: stats

--- src/components/MapRender/index.tsx
+++ src/components/MapRender/index.tsx
@@ -1,3 +1,4 @@
+import { Stats } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 import MapBackground from '../../components/MapBackground';
@@ -6,22 +7,25 @@ import MapHelper from '../../components/MapHelper';
 
 function MapRender() {
   return (
-    <div id="canvas-container">
-      <Canvas
-        className="canvas-map"
-        orthographic
-        camera={{
-          position: [0, 0, 50],
-          zoom: 1,
-          up: [0, 0, 1],
-        }}
-        flat
-      >
-        <MapBackground />
-        {process.env.NODE_ENV === 'development' && <MapHelper />}
-        <MapControlsHelper />
-      </Canvas>
-    </div>
+    <>
+      <div id="canvas-container">
+        <Canvas
+          className="canvas-map"
+          orthographic
+          camera={{
+            position: [0, 0, 50],
+            zoom: 1,
+            up: [0, 0, 1],
+          }}
+          flat
+        >
+          <MapBackground />
+          {process.env.NODE_ENV === 'development' && <MapHelper />}
+          <MapControlsHelper />
+        </Canvas>
+      </div>
+      {process.env.NODE_ENV === 'development' && <Stats className="fps" />}
+    </>
   );
 }
 
